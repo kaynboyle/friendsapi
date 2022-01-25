b2b660cd6c0c69f8f2e2168a68137e7490a78db0
Kaylin Boyle <kaylinnboyle@gmail.com>
2022-01-24 21:16:47 -0800

    save

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -72,7 +72,7 @@ const userController = {
                 }
                 res.json(dbUserData);
             })
-            .catch(err => res.status(400).json(err));
+            .catch(err => res.status(400).json(err)); 
     }
 };
 
